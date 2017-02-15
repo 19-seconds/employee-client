477c54777ba9422ca7452cbc8d7ed986ecc421b6
Salomon Beza <beza.salomon@gmail.com>
2017-02-15 20:13:40 +0200

    created create and index actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,18 @@ def show
   @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").body
 end
 
+def index
+  @employees = Unirest.get("http://localhost:3000/employees.json").body
+end
+
+def create
+  @employee = Unirest.post("http://localhost:3000/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
+
+  redirect_to employee_path(@employee["id"])
+end
+
+def new
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get "employees/:id", to:'employees#show'
+ post "/employees", to:'employees#create'
+ 
+  get "/employees", to:'employees#index'
+  get "/employees/new", to: 'employees#new'
+   get "/employees/:id", to:'employees#show'
 end
