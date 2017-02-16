e1ad3b557c28b086424803a97e603fc2727f5c6f
Mark Richardson <markeban@yahoo.com>
2017-02-16 15:35:52 +0200

    finished RESTful actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,19 +1,34 @@
 class EmployeesController < ApplicationController
-def show
-  @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
-end
+  
+  def show
+    @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
+  end
 
-def index
-  @employees = Unirest.get("#{ENV['DOMAIN']}/employees.json").body
-end
+  def index
+    @employees = Unirest.get("#{ENV['DOMAIN']}/employees.json").body
+  end
 
-def create
-  @employee = Unirest.post("#{ENV['DOMAIN']}/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
-  redirect_to "/employees/#{@employee['id']}"
-end
+  def new
+  end
 
-def new
+  def create
+    @employee = Unirest.post("#{ENV['DOMAIN']}/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
 
-end
+  def edit
+    @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
+  end
+
+  def update
+    @employee = Unirest.patch("#{ENV['DOMAIN']}/employees/#{params[:id]}.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def destroy
+    Unirest.delete("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
+    redirect_to "/employees"
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
- post "/employees", to:'employees#create'
- 
+
+
+  post "/employees", to:'employees#create'
   get "/employees", to:'employees#index'
   get "/employees/new", to: 'employees#new'
-   get "/employees/:id", to:'employees#show'
+  get "/employees/:id", to:'employees#show'
+  get "/employees/:id/edit", to: 'employees#edit'
+  patch "/employees/:id", to: 'employees#update'
+  delete "/employees/:id", to: 'employees#destroy'
 end
