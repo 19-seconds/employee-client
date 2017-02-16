a0175c7bb60f40753149f49a8a213893916fb469
Mark Richardson <markeban@yahoo.com>
2017-02-16 11:46:01 +0200

    changed to namespaced api routes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,15 @@
 class EmployeesController < ApplicationController
 def show
-  @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").body
+  @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
 end
 
 def index
-  @employees = Unirest.get("http://localhost:3000/employees.json").body
+  @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
 end
 
 def create
-  @employee = Unirest.post("http://localhost:3000/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
-
-  redirect_to :index
+  @employee = Unirest.post("http://localhost:3000/api/v1/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
+  redirect_to "/employees/#{@employee['id']}"
 end
 
 def new
