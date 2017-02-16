46c97470e94dc44ee3147af384505ebef082293a
Mark Richardson <markeban@yahoo.com>
2017-02-16 13:52:04 +0200

    added env variables with dot_env gem to DRY code

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,14 +1,14 @@
 class EmployeesController < ApplicationController
 def show
-  @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+  @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
 end
 
 def index
-  @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+  @employees = Unirest.get("#{ENV['DOMAIN']}/employees.json").body
 end
 
 def create
-  @employee = Unirest.post("http://localhost:3000/api/v1/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
+  @employee = Unirest.post("#{ENV['DOMAIN']}/employees.json", :headers => {"Accept"=> "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email=> params[:email], :birthdate => params[:birthdate], :ssn => params[:ssn]}).body
   redirect_to "/employees/#{@employee['id']}"
 end
 
